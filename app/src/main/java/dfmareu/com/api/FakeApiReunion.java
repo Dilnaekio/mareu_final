14c7158737c9a2dd8c847802cd1c8e3e233ab7c8
Fabian Delcourt <delcourtfabian@gmail.com>
2021-04-09 13:40:46 +0200

    Remise en état de la fake list

--- app/src/main/java/dfmareu/com/api/FakeApiReunion.java
+++ app/src/main/java/dfmareu/com/api/FakeApiReunion.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 
 import dfmareu.com.models.Reunion;
 
-import static dfmareu.com.api.FakeApiServiceGenerator.FakeReunions;
+import static dfmareu.com.api.FakeApiServiceGenerator.generateReunion;
 
 
 public class FakeApiReunion implements ApiReunion{
 
     //private final ArrayList<Reunion> reunions = new ArrayList<>();
     //To test with fake list
-    private final ArrayList<Reunion> reunions = FakeReunions;
+    private final ArrayList<Reunion> reunions = generateReunion();
 
     public FakeApiReunion() {
     }
