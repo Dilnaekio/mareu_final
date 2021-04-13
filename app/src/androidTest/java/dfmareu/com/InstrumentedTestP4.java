5e8ddd8d2615cf349801dc8006eac32c0edadb80
Fabian Delcourt <delcourtfabian@gmail.com>
2021-04-13 19:45:36 +0200

    - Ajustement de la précision des tests instrumentalisés

--- app/src/androidTest/java/dfmareu/com/InstrumentedTestP4.java
+++ app/src/androidTest/java/dfmareu/com/InstrumentedTestP4.java
@@ -1,12 +1,7 @@
 package dfmareu.com;
 
-import android.util.Log;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
-
-import androidx.recyclerview.widget.RecyclerView;
-
-import static dfmareu.com.RecyclerViewItemCountAssertion.withItemCount;
 
 import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
@@ -17,18 +12,11 @@ import androidx.test.filters.LargeTest;
 
 import org.hamcrest.Matchers;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.ArrayList;
-
-import dfmareu.com.api.FakeApiServiceGenerator;
-import dfmareu.com.di.Injection;
-import dfmareu.com.models.Reunion;
-import dfmareu.com.repository.ReunionRepository;
 import dfmareu.com.ui.create.CreateReunion;
 import dfmareu.com.ui.main.MainActivity;
 
@@ -38,12 +26,12 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static dfmareu.com.Matchers.RecyclerViewItemAtPositionAssertion.atPosition;
 import static org.hamcrest.core.AllOf.allOf;
 
 /**
@@ -56,19 +44,10 @@ import static org.hamcrest.core.AllOf.allOf;
 public class InstrumentedTestP4 {
     @Rule
     public ActivityScenarioRule<MainActivity> activityActivityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
-    ReunionRepository reunionRepository;
 
     @Before
     public void setUp() {
         Intents.init();
-        reunionRepository = Injection.createReunionRepository();
-
-        int index = 0;
-        ArrayList<Reunion> fakeReunions = FakeApiServiceGenerator.FakeReunions;
-        while (index < fakeReunions.size()) {
-            reunionRepository.addReunion(fakeReunions.get(index));
-            index++;
-        }
     }
 
     @After
@@ -82,55 +61,48 @@ public class InstrumentedTestP4 {
         onView(withId(R.id.fab)).perform(click());
         intended(hasComponent(CreateReunion.class.getName()));
 
-        //TEST FOR SUBJECT (Create Reunion)
+        //SUBJECT (Create Reunion)
         onView(withId(R.id.activity_create_reunion_SubjectEditTxt))
                 .perform(typeText("Sujet test"));
 
-        //TEST FOR MAIL PARTICIPANTS (Create Reunion)
+        //PARTICIPANTS (Create Reunion)
         onView(withId(R.id.activity_create_reunion_ParticipantEditTxt))
                 .perform(typeText("test@gmail.com"));
         onView(withId(R.id.activity_create_reunion_participants_AcceptBtn))
                 .perform(click());
-        onView(withId(R.id.activity_create_reunion_recycler_view))
-                .perform(RecyclerViewActions.scrollTo(hasDescendant(withText("test@gmail.com"))));
 
-        //TEST FOR DATE SELECTION (Create Reunion)
+        //DATE SELECTION (Create Reunion)
         onView(withId(R.id.activity_create_reunion_DateBtn))
                 .perform(click());
         onView(withClassName(Matchers.equalTo(DatePicker.class.getName())))
                 .perform(PickerActions.setDate(2021, 4, 15));
         onView(withText("OK")).perform(click());
-        onView(withId(R.id.activity_create_reunion_SelectedDate))
-                .check(matches(withText("15/04/2021")));
 
-        //TEST FOR TIME SELECTION (Create Reunion)
+        //TIME SELECTION (Create Reunion)
         onView(withId(R.id.activity_create_reunion_TimeBtn))
                 .perform(click());
         onView(withClassName(Matchers.equalTo(TimePicker.class.getName())))
                 .perform(PickerActions.setTime(15, 15));
         onView(withText("OK")).perform(click());
-        onView(withId(R.id.activity_create_reunion_SelectedTime))
-                .check(matches(withText("15h15")));
 
         //RETURN ON THE MAIN ACTIVITY
         onView(withId(R.id.fabValidate))
                 .perform(click());
 
-        //CHECK IF THE ONLY (and new) REUNION WITH "delcourtfabian@gmail.com" is visible in the main recyclerview
+        //CHECK IF THE ONLY (and new) REUNION WITH "test@gmail.com" is visible in the main recyclerview
         onView(withId(R.id.activity_main_recycler_view))
                 .perform(RecyclerViewActions.scrollTo(hasDescendant(withText("test@gmail.com"))));
     }
 
     @Test
     public void testDeleteReunion() {
-        //TEST TO DELETE ONE REUNION (Begins with 10 reunions)
-        int expectedList = 9;
+        //TEST TO DELETE ONE REUNION
         onView(allOf(withId(R.id.activity_main_item_garbage),
                 withParent(
-                        allOf(withId(R.id.activity_main_item_first_Linear), hasDescendant(withText("Salle A"))))))
+                        allOf(withId(R.id.activity_main_item_first_Linear), hasDescendant(withText("Salle B"))))))
                 .perform(click());
-        ArrayList<Reunion> actualList = reunionRepository.getReunions();
-        onView(withId(R.id.activity_main_recycler_view)).check(withItemCount(expectedList));
+        onView(withId(R.id.activity_main_recycler_view))
+                .check(matches(atPosition(1, hasDescendant(withText("Salle C")))));
     }
 
     @Test
---
+++ app/src/androidTest/java/dfmareu/com/Matchers/RecyclerViewItemAtPositionAssertion.java
@@ -0,0 +1,50 @@
+package dfmareu.com.Matchers;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.ViewAssertion;
+import androidx.test.espresso.matcher.BoundedMatcher;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
+import static androidx.test.espresso.intent.Checks.checkNotNull;
+
+
+public class RecyclerViewItemAtPositionAssertion implements ViewAssertion {
+
+    public static Matcher<View> atPosition(final int position, @NonNull final Matcher<View> itemMatcher) {
+        checkNotNull(itemMatcher);
+        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("has item at position " + position + ": ");
+                itemMatcher.describeTo(description);
+            }
+
+            @Override
+            protected boolean matchesSafely(final RecyclerView view) {
+                RecyclerView.ViewHolder viewHolder = view.findViewHolderForAdapterPosition(position);
+                if (viewHolder == null) {
+                    // has no item on such position
+                    return false;
+                }
+                return itemMatcher.matches(viewHolder.itemView);
+            }
+        };
+    }
+
+    private RecyclerViewItemAtPositionAssertion() {
+    }
+
+    @Override
+    public void check(View view, NoMatchingViewException noViewFoundException) {
+        if (noViewFoundException != null) {
+            throw noViewFoundException;
+        }
+    }
+}
+
